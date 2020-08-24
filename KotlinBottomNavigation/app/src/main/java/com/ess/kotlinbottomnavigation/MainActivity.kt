3b825c1778431ea986c428bb48db08e52f5daa07
EbubekirSezer <ebubekirssezer@gmail.com>
2020-08-24 19:01:22 +0300

    Kotlin bottom navigaiton bar sample upload

---
+++ KotlinBottomNavigation/app/src/main/java/com/ess/kotlinbottomnavigation/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.ess.kotlinbottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        setupBottomNavMenu(navController)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav?.setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.nav_host_fragment)) || super.onOptionsItemSelected(item)
+    }
+}
--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 These are the kotlins samples that i create for the my future works
 
 
-### Kotlin Navigation Drawer         
+## Kotlin Navigation Drawer         
 
 ![Kotlin Navigation Drawer](images/navdrawer2.gif)  
+
+## Kotlin Navigation Bottom Bar         
+
+![Kotlin Navigation Bottom Bar](images/kotlinbottomnavigation.gif)  
